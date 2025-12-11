b3c832b2a1204499e451132478fb7c36abd81fc6
HaLdeR31233 <135335307+HaLdeR31233@users.noreply.github.com>
2025-12-11 18:57:08 +0200

    Update HomePageController.php

--- src/classes/HomePageController.php
+++ src/classes/HomePageController.php
@@ -1,27 +1,21 @@
-﻿<?php
+<?php
 namespace App\Classes;
 
-use Monolog\Logger;
-use Carbon\Carbon;
 
 class HomePageController
 {
     private Viewer $viewer;
-    private Logger $logger;
 
-    public function __construct(Viewer $viewer, Logger $logger)
+    public function __construct(Viewer $viewer)
     {
         $this->viewer = $viewer;
-        $this->logger = $logger;
     }
 
     public function index(): void
     {
-        $this->logger->info("Home page accessed");
-
         $data = [
             "title" => "Головна сторінка - DIM.RIA",
-            "currentTime" => Carbon::now()->format("H:i:s d.m.Y"),
+            "currentTime" => date("H:i:s d.m.Y"),
             "message" => "Ласкаво просимо до DIM.RIA!"
         ];
 
